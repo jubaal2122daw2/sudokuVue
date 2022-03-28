9c97a8caf63592774e64fe17776f4c6cf471c8eb
jubaal <jubaal2122daw2@protonmail.com>
2022-03-28 18:58:45 +0200

    muestra los valores en una tabla

--- src/main.mjs
+++ src/main.mjs
@@ -1,18 +1,54 @@
 //en este script se van a llamar a los componentes. Y será eñ menu lateral.
 
-import { Tablero } from "./clases.mjs";
-import {TableroComponent} from "./components/tablero-component.js";
+//import { Tablero } from "./clases.mjs";
+//import {TableroComponent} from "./components/tablero-component.js";
 
+let sudoku = [
+  [1,2,3,4,5,6,7,8,9],
+  [5,6,7,8,9,1,2,3,4],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+]
+
+Vue.component('FilaComponent',{
+  props: ['fila'],
+  template: `
+    <tr>
+      <td>{{fila[0]}}</td>
+      <td>{{fila[1]}}</td>
+      <td>{{fila[2]}}</td>
+      <td>{{fila[3]}}</td>
+      <td>{{fila[4]}}</td>
+      <td>{{fila[5]}}</td>
+      <td>{{fila[6]}}</td>
+      <td>{{fila[7]}}</td>
+      <td>{{fila[8]}}</td>
+   </tr>
+  `
+});
+
+Vue.component('TableroComponent', {
+  data: function(){
+    return {
+      sudoku: sudoku
+    };
+  },
+  template: `
+  <table>
+      <FilaComponent v-for='index in 9' v-bind:fila='sudoku[index-1]'/>
+  </table>`
+});
 
 let app = new Vue({
   el: '#app',
   data: {
     prueba: 'hola'
   },
-  components: {
-    TableroComponent
-  },
-  
   template: `
   <div>
     <h1>{{prueba}}</h1>
